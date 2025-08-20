fb8fff447193914edac864317259f2843d06280f
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2025-08-21 00:21:59 +0600

    fix

--- src/router/Route.jsx
+++ src/router/Route.jsx
@@ -1,8 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../home/Home";
-import ForgotPassword from "../auth/forgotpassword";
-import VerifyOtp from "../auth/VerifyOtp";
-import UpdatePassword from "../auth/UpdatePassword";
+ 
 import Main from "../layout/Main";
  
 
@@ -15,19 +13,7 @@ export const router = createBrowserRouter([
         path: "/",
         element: <Home></Home>,
         errorElement: <ErrorPage />
-    },
-    {
-        path: "forgotpassword",
-        element: <ForgotPassword></ForgotPassword>
-    },
-    {
-        path: "verifyotp",
-        element: <VerifyOtp></VerifyOtp>
-    },
-    {
-        path: "updatepassword",
-        element: <UpdatePassword />
-    },
+    }, 
 
     {
         path: "dashboard",
