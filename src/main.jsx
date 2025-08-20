f7a25c7074b93ea82681fdd940e15bc39c99235b
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2025-08-21 00:19:22 +0600

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,17 @@ import Home from "./home/Home.jsx";
 
 import { RouterProvider } from "react-router-dom";
  
-import { Provider } from "react-redux";
+ 
 import { router } from "./router/Route.jsx";
  
  
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <Provider store={store}>  */}
+ 
     <RouterProvider router={router}>
       <Home></Home>
     </RouterProvider>
-    {/* </Provider> */}
+ 
   </StrictMode>
 );
